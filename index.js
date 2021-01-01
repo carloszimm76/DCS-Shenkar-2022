368223b4195e1cad6d810392ea9d403d134a97b5
carloszimm76 <a85620422@sina.com>
2021-01-02 03:28:09 +0800

    add adi name

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const http = require ("http");
+const http = require("http");
 const port = 8080;
 
 http.createServer((req, res) => {
-	res.writeHead(200); // Status code in header
-	res.write("We built a server"); // Send res body (can write multiple times)
-	res.end(); // Send & close connection
+    res.writeHead(200); // Status code in header
+    res.write("adi peled"); // Send res body (can write multiple times)
+    res.end(); // Send & close connection
 }).listen(port); // Listen for connection on this port
 
 console.log(`Listening on port ${port}`);
