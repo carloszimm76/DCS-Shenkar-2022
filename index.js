6b8a503bb713671aad3365b83805a3ebc672f1e8
carloszimm76 <a85620422@sina.com>
2021-01-06 06:22:10 +0800

    Changed the write to Michael Struk

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 8080;
 
 http.createServer((req, res) => {
     res.writeHead(200); // Status code in header
-    res.write("adi peled"); // Send res body (can write multiple times)
+    res.write("Michael Struk"); // Send res body (can write multiple times)
     res.end(); // Send & close connection
 }).listen(port); // Listen for connection on this port
 
