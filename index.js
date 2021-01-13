779b9203af1b2fca6080cb45ff4a70c3ba52578a
carloszimm76 <a85620422@sina.com>
2021-01-14 02:57:43 +0800

    Yosef

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const http = require("http");
+const http = require ("http");
 const port = 8080;
 
 http.createServer((req, res) => {
-    res.writeHead(200); // Status code in header
-    res.write("Ehab Damouny "); // Send res body (can write multiple times)
-    res.end(); // Send & close connection
+	res.writeHead(200); // Status code in header
+	res.write("Yosef Aranbayev"); // Send res body (can write multiple times)
+	res.end(); // Send & close connection
 }).listen(port); // Listen for connection on this port
 
 console.log(`Listening on port ${port}`);
