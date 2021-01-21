c0268a0d47ed26df277058e3e29e4d274a29df00
carloszimm76 <a85620422@sina.com>
2021-01-21 13:07:16 +0800

    Added middleware parms cheking

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const express = require('express');
+const { resourceUsage } = require('process');
+const stringcheker = require('./stringcheker');
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Middleware section
+app.param('githubUserName', (req, res, next,value) => {
+    const githubUserName = value;
+    if(stringcheker.checkStringIfHasSpacesOrNumbers(githubUserName)){
+        // res.send(400, 'Invalid github username');
+        res.status(400).send('Invalid github username');
+    }
+    else{
+        next()
+    }
+    
+  });
+app.param('repoName', (req, res, next,value) => {
+    const repoName = value;
+    if(stringcheker.checkStringIfHasSpacesOrNumbers(repoName)){
+        // res.send(400, 'Invalid repo name');
+        res.status(400).send('Invalid repo name');
+    }
+    else{
+        next()
+    } 
+});
 
+//Routing section
 app.get('/api/v1/githubUser/:githubUserName/avatar', (req, res) => {
     res.send(200, 'OK');
 });
---
+++ stringcheker.js
@@ -0,0 +1,14 @@
+
+const containsNumbers = (str) =>{
+    var regexp = /\d/g;
+    return regexp.test(str);
+};
+
+
+const checkStringIfHasSpacesOrNumbers = (str) =>{
+    if(str.indexOf(' ') >= 0){
+        return true;
+    }
+    return containsNumbers(str);
+}
+module.exports = {checkStringIfHasSpacesOrNumbers}
